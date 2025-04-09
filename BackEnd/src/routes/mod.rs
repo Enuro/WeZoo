87e04d9ca54b2de11d6813441e6a4ba0520372be
Enuro <alpha9a@mail.ru>
2025-04-09 14:54:20 +0300

    sex mashine

--- BackEnd/src/routes/mod.rs
+++ BackEnd/src/routes/mod.rs
@@ -2,3 +2,4 @@ pub mod home_routes;
 pub mod handlers;
 pub mod auth_routes;
 pub mod user_routes;
+pub mod middlewares;
--- BackEnd/src/routes/user_routes.rs
+++ BackEnd/src/routes/user_routes.rs
@@ -1,10 +1,13 @@
-use actix_web::web;
+use actix_web::{middleware::from_fn, web};
+use actix_web_lab::middleware;
 
-use super::handlers;
+use super::{handlers, middlewares};
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg
     .service(
         web::scope("/user")
+        .wrap(from_fn(middlewares::auth_middlewares::check_auth_middleware))
+        .service(handlers::user_handlers::user)
     );
 }
